7449fb1a990a51476b36a3c85665c4aec79aa0c0
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-12-23 15:00:06 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# The Kilonova Code (KNC)
+_Simulates relativistic jets, winds, and explosions in an axisymmetric setting._
+
+KNC is written and maintained by the [Computational Astrophysics Lab](https://jzrake.people.clemson.edu) at the [Clemson University Department of Physics and Astronomy](http://www.clemson.edu/science/departments/physics-astro). The core developer/maintainer is presently Jonathan Zrake.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cdc_tools",
+    version="0.1.0",
+    author="J. Zrake",
+    author_email="jzrake@clemson.edu",
+    description="Python module to work with data output from the Kilonova code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clemson-cal/app-kilonova",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'h5py'],
+    python_requires='>=3.6',
+)
