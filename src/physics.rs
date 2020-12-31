beb93d3fdfa99f4278e713f8a57e7642f4efab52
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-12-31 10:49:38 -0500

    Workaround serde_pickle bug breaking checkpoints
    The workaround is to tag unit variants for the
    riemann_solver and runge_kutta_order options.
    Hopefully this can be reverted when serde_pickle
    is fixed:
    
    https://github.com/birkenfeld/serde-pickle/issues/11

--- src/physics.rs
+++ src/physics.rs
@@ -25,7 +25,7 @@ pub enum Direction {
  * Enum for Riemann solver type
  */
 #[derive(Clone, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
+#[serde(rename_all = "snake_case", tag = "type")]
 pub enum RiemannSolver {
     Hlle,
     Hllc,
