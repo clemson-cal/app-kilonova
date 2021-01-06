58ba3690dea5f08423c35b293332fe1c6b42c7c9
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-01-06 11:38:59 -0500

    Make serde_cbor and snap features to improve build times

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,7 @@ mod traits;
  */
 use std::{
     ffi::OsStr,
-    fs::{
-        File,
-        read_to_string,
-    },
-    io::BufWriter,
-    io::BufReader,
+    fs::read_to_string,
     path::Path,
 };
 use serde::{
@@ -285,28 +280,51 @@ fn parent_directory(path_str: &str) -> String {
     }.into()
 }
 
+#[cfg(feature = "serde_cbor")]
 fn write_cbor<T: Serialize>(value: &T, path_str: &str, snappy_compression: bool) -> anyhow::Result<()> {
     println!("write {}", path_str);
-    let file = File::create(&path_str)?;
-    let buffer = BufWriter::new(file);
+    let file = std::fs::File::create(&path_str)?;
+    let buffer = std::io::BufWriter::new(file);
 
     if snappy_compression {
-        serde_cbor::to_writer(snap::write::FrameEncoder::new(buffer), &value)?;
+        #[cfg(feature = "snap")] {
+            serde_cbor::to_writer(snap::write::FrameEncoder::new(buffer), &value)?;
+        }
+        #[cfg(not(feature = "snap"))] {
+            anyhow::bail!("snappy_compression = true, but snap is not enabled")
+        }
     } else {
         serde_cbor::to_writer(buffer, &value)?;        
     }
     Ok(())
 }
 
+#[cfg(not(feature = "serde_cbor"))]
+fn write_cbor<T: Serialize>(_: &T, path_str: &str, _: bool) -> anyhow::Result<()> {
+    println!("skip writing {} (serde_cbor is not enabled)", path_str);
+    Ok(())
+}
+
+#[cfg(feature = "serde_cbor")]
 fn read_cbor<T: for<'de> Deserialize<'de>>(path_str: &str, snappy_compression: bool) -> anyhow::Result<T> {
-    let file = File::open(path_str)?;
-    let buffer = BufReader::new(file);
+    let file = std::fs::File::open(path_str)?;
+    let buffer = std::io::BufReader::new(file);
 
     if snappy_compression {
-        Ok(serde_cbor::from_reader(snap::read::FrameDecoder::new(buffer))?)
+        #[cfg(feature = "snap")] {
+            Ok(serde_cbor::from_reader(snap::read::FrameDecoder::new(buffer))?)
+        }
+        #[cfg(not(feature = "snap"))] {
+            anyhow::bail!("input file is compressed, but snap is not enabled")
+        }
     } else {
-        Ok(serde_cbor::from_reader(buffer)?)        
+        Ok(serde_cbor::from_reader(buffer)?)
     }
+}
+
+#[cfg(not(feature = "serde_cbor"))]
+fn read_cbor<T: for<'de> Deserialize<'de>>(path_str: &str, _: bool) -> anyhow::Result<T> {
+    anyhow::bail!("input file {} given, but serde_cbor is not enabled", path_str)
 }
 
 
