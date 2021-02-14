20ca03b0de6d06b3a1909f8ac58763104415947f
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-02-13 19:48:06 -0500

    Switch from serde_cbor -> ciborium for compile time
    The --release build went from 30s to 10s by dropping
    serde_cbor in favor of ciborium. The load time is
    just marginally (~20%) slower with ciborium. I don't
    know why the serde_cbor build times were so bad. I
    hope the ciborium authors plan to maintain it.

--- src/io.rs
+++ src/io.rs
@@ -8,9 +8,11 @@ use serde::{Serialize, Deserialize};
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
 
-    #[cfg(feature = "serde_cbor")]
     #[error("{0}")]
-    SerdeCbor(#[from] serde_cbor::Error),
+    CiboriumSer(#[from] ciborium::ser::Error<std::io::Error>),
+
+    #[error("{0}")]
+    CiboriumDe(#[from] ciborium::de::Error<std::io::Error>),
 
     #[error("{0}")]
     IO(#[from] std::io::Error),
@@ -37,30 +39,15 @@ pub fn parent_directory(path_str: &str) -> String {
     }.into()
 }
 
-#[cfg(feature = "serde_cbor")]
 pub fn write_cbor<T: Serialize>(value: &T, path_str: &str) -> Result<(), Error> {
     println!("write {}", path_str);
     let file = std::fs::File::create(&path_str)?;
-    let buffer = std::io::BufWriter::new(file);
-
-    serde_cbor::to_writer(buffer, &value)?;        
-    Ok(())
+    let mut buffer = std::io::BufWriter::new(file);
+    Ok(ciborium::ser::into_writer(&value, &mut buffer)?)
 }
 
-#[cfg(not(feature = "serde_cbor"))]
-pub fn write_cbor<T: Serialize>(_: &T, path_str: &str) -> Result<(), Error> {
-    println!("skip writing {} (serde_cbor is not enabled)", path_str);
-    Ok(())
-}
-
-#[cfg(feature = "serde_cbor")]
 pub fn read_cbor<T: for<'de> Deserialize<'de>>(path_str: &str) -> Result<T, Error> {
     let file = std::fs::File::open(path_str)?;
     let buffer = std::io::BufReader::new(file);
-    Ok(serde_cbor::from_reader(buffer)?)
-}
-
-#[cfg(not(feature = "serde_cbor"))]
-pub fn read_cbor<T: for<'de> Deserialize<'de>>(path_str: &str) -> Result<T, Error> {
-    Err(Error::SerdeCborNotEnabled(path_str.to_string()))
+    Ok(ciborium::de::from_reader(buffer)?)
 }
