9131ada6d3c1d094a3317c480de11c432dd83c84
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-02-22 12:16:08 -0500

    Whitespace

--- src/state.rs
+++ src/state.rs
@@ -133,7 +133,7 @@ impl<C: Conserved> runge_kutta::WeightedAverage for BlockState<C> {
         let c0 = s0.scalar_mass.clone();
         let c1 = s1.scalar_mass.clone();
 
-        Self{
+        Self {
             conserved:   u1 * (-bf + 1.) + u0 * bf,
             scalar_mass: c1 * (-bf + 1.) + c0 * bf,
         }
@@ -151,7 +151,7 @@ impl<C: Conserved> runge_kutta::WeightedAverage for State<C> {
             .into_iter()
             .map(|(index, s1)| (index, s1.weighted_average(br, &s0.solution[&index])));
 
-        Self{
+        Self {
             time:      self.time      * (-bf + 1.) + s0.time      * bf,
             iteration: self.iteration * (-br + 1 ) + s0.iteration * br,
             solution: s_avg.into_iter().collect(),
@@ -184,7 +184,7 @@ impl<C: Conserved> runge_kutta::WeightedAverageAsync for State<C> {
             }
         });
 
-        Self{
+        Self {
             time:      self.time      * (-bf + 1.) + s0.time      * bf,
             iteration: self.iteration * (-br + 1 ) + s0.iteration * br,
             solution: join_all(s_avg).await.into_iter().collect(),
