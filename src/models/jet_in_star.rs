ecc11e2d2ab0bcc903a3d3d607236e9d11d25cd6
sagar <sagara@clemson.edu>
2021-03-28 20:23:25 -0400

    Make up to date with clemsoncal with modification to  wind_shock setups

--- src/models/jet_in_star.rs
+++ src/models/jet_in_star.rs
@@ -6,7 +6,7 @@ use crate::traits::InitialModel;
 
 
 
-// static UNIFORM_TEMPERATURE: f64 = 1e-6;
+static UNIFORM_TEMPERATURE: f64 = 1e-10;
 
 // Constants as given in Duffell & MacDayen(2015)
 // source: https://arxiv.org/pdf/1407.8250.pdf
@@ -20,8 +20,9 @@ static K1:                  f64 = 3.24;
 static K2:                  f64 = 2.57;
 static N:                   f64 = 16.7;
 static RHO_WIND:            f64 = 1e-9 * M0 / (1.33 * PI * R0 * R0 * R0);
-static RHO_ENV:             f64 = 1e-7 * M0 / (1.33 * PI * R0 * R0 * R0);
+// static RHO_ENV:             f64 = 1e-7 * M0 / (1.33 * PI * R0 * R0 * R0);
 static R_NOZZ:              f64 = 0.01 * R0; 
+static R_ENV:               f64 = 1.2  * R0;
 static ALPHA:               f64 = 2.5;
 
 
@@ -53,8 +54,11 @@ pub struct JetInStar {
     /// Radius of the Envelope
     pub envelope_radius: f64,
 
-    /// Energy-to-mass ratio of progenitor
-    pub eta_0: f64, 
+    /// Mass of the Envelope 
+    pub envelope_mass: f64,
+
+    /// Hydrogen Volume Filling Factor
+    pub volume_factor: f64,
 }
 
 
@@ -84,7 +88,7 @@ impl InitialModel for JetInStar {
         let (r, q) = coordinate;
         let d = self.mass_density(r, q, t);
         let u = self.gamma_beta(r, q, t);
-        let p = 1e-10;
+        let p = d * UNIFORM_TEMPERATURE;
 
         AnyPrimitive {
             velocity_r: u,
@@ -96,12 +100,13 @@ impl InitialModel for JetInStar {
 
     fn scalar_at(&self, coordinate: (f64, f64), t: f64) -> f64 {
         let (r, q) = coordinate;
+        let zone   = self.zone(r, q, t);
 
-        match self.zone(r, q, t) {
-            Zone::Core     => 1.0,
-            Zone::Jet      => 1.0,
-            Zone::Envelope => 1e2,
-            Zone::Wind     => 1.0,
+        match zone {
+            Zone::Core     => 1e+0,
+            Zone::Jet      => 1e+2,
+            Zone::Envelope => 1e-2 * (r / R3).powf(-2.0),
+            Zone::Wind     => 1e-5 * (r / R_ENV).powf(-2.0),
         }
     }
 }
@@ -112,33 +117,29 @@ impl InitialModel for JetInStar {
 // ============================================================================
 impl JetInStar
 {
-
     /**
      * The comoving mass density in g/cc
      */
     fn mass_density(&self, r: f64, q: f64, t: f64) -> f64{
         let zone      = self.zone(r, q, t);
-        let num       = RHO_C * (1.0 - r / R3).powf(N);
+        let num       = RHO_C * ((1.0 - r / R3)).powf(N);
         let denom     = 1.0 + (r / R1).powf(K1) / (1.0 + (r / R2).powf(K2));
-        let core_zone = num/denom;
-
-        // To ensure continuity in the density from one zone to another we add
-        // their contributions until each zone "falls off"
+        let core_zone = num / denom;
+        let rho_env   = self.envelope_mass / (4.0 * PI * self.envelope_radius.powi(2) * (self.envelope_radius - R3) * self.volume_factor);
 
         match zone {
             Zone::Core => {
-                core_zone + RHO_ENV * (r / R3).powf(-ALPHA) + RHO_WIND * (r / self.envelope_radius).powf(-2.0)
+                core_zone + rho_env * (r/R3).powf(-2.0)
             }
             Zone::Envelope => {
-                RHO_ENV * (r / R3).powf(-ALPHA) + RHO_WIND * (r / self.envelope_radius).powf(-2.0)
+                rho_env *(r/R3).powf(-ALPHA)
             }
             Zone::Jet => {
-                self.jet_mass_rate_per_steradian() / (r * r * self.engine_u * LIGHT_SPEED)
+                self.jet_mass_rate_per_steradian(r, q) / (r * r * self.engine_u * LIGHT_SPEED)
             }
             Zone::Wind => {
-                RHO_WIND * (r / self.envelope_radius).powf(-2.0)
+                RHO_WIND * (r/R_ENV).powf(-2.0)
             }
-            
         }
     }
 
@@ -178,14 +179,13 @@ impl JetInStar
     pub fn zone(&self, r: f64, q: f64, t: f64) -> Zone {
         let v_jet = self.engine_beta() * LIGHT_SPEED;
         let r_jet_head = v_jet * t;
-        let r_jet_tail = v_jet * (t - self.engine_duration);
 
-        if self.in_nozzle(q) && (r_jet_tail..r_jet_head).contains(&r) {
+        if self.in_nozzle(q) && r < r_jet_head {
             Zone::Jet
         } else if r < R3 {
             Zone::Core
-        } else if r < self.envelope_radius {
-            Zone::Envelope
+        } else if R3 < r && r < self.envelope_radius {
+            Zone:: Envelope
         } else {
             Zone::Wind
         }
@@ -202,6 +202,7 @@ impl JetInStar
         match self.zone(r, q, t) {
             Zone::Jet => self.engine_u,
             _ => 0.0
+
         }
     }
 
@@ -213,23 +214,24 @@ impl JetInStar
      * * `q` - The polar angle theta
      */
     pub fn nozzle_function(&self, r: f64, q: f64) -> f64 {
-
         // Normalize the Nozzle Radius
         let r0 = R_NOZZ/R0;
         let q2 = self.engine_theta.powi(2);
 
-        // Nozzle Function Normalization Factor: N0 = 4 * PI * r0^3 * exp(-2/theta0^2) * theta0^2
+        // Nozzle Function Normalization Factor
+        // N0 = 4 * PI * r0^3 * exp(-2/theta0^2) * theta0^2
         let n_0 =  4.0 * PI * r0 * r0 * r0 * (1.0 - (-2.0 / q2).exp()) * q2;
 
-        // Nozzle Function: g = (r/r0) * exp(-(r/r0)^2 / 2) * exp[(cos^2(q) - 1)/theta0^2] / N0
+        // Nozzle Function: g = (r/r0) * exp(-(r/r0)^2) * exp[(cos^2(q) - 1)/theta0^2] / N0
         let g = (r / R_NOZZ) * f64::exp(-(r / R_NOZZ).powf(2.0) / 2.0) * f64::exp((q.cos().powf(2.0) - 1.0) / q2);
+
         g / n_0
     }
 
-    fn jet_mass_rate_per_steradian(&self) -> f64 {
+    fn jet_mass_rate_per_steradian(&self, r: f64, q: f64) -> f64 {
         let engine_gamma = f64::sqrt(1.0 + self.engine_u * self.engine_u);
         let e = self.engine_energy;
-        let l = e / (self.eta_0 * 4.0 * PI * self.engine_duration);
+        let l = self.nozzle_function(r, q) * e / (4.0 * PI * self.engine_duration);
         l / (engine_gamma * LIGHT_SPEED * LIGHT_SPEED)
     }
 }
--- src/models/wind_shock.rs
+++ src/models/wind_shock.rs
@@ -1,12 +1,9 @@
-use std::f64::consts::PI;
-use serde::{Deserialize, Serialize};
 use crate::physics::{AnyPrimitive, LIGHT_SPEED};
 use crate::traits::InitialModel;
+use serde::{Deserialize, Serialize};
+use std::f64::consts::PI;
 
 static UNIFORM_TEMPERATURE: f64 = 1e-6;
-
-
-
 
 /**
  * Jet propagating through a kilonova debris cloud and surrounding relativistic
@@ -18,18 +15,30 @@ pub struct WindShock {
     /// Rate of outflow of the wind
     pub wind_mass_outflow_rate: f64,
 
+    /// Rate of outflow of the flare
+    pub flare_outflow_rate: f64,
+
+    /// Four velocity of flare
+    pub flare_gamma_beta: f64,
+
     /// Four velocity of wind
     pub wind_gamma_beta: f64,
 
     /// Four velocity of wind after shock
     pub post_shock_gamma_beta: f64,
 
+    /// Wind pressure
+    pub wind_pressure: f64,
+
+    /// Pressure after shock
+    pub post_shock_pressure: f64,
+
     /// Shock location coordinate
     pub shock_location: f64,
+
+    /// Flare time
+    pub flare_time: f64,
 }
-
-
-
 
 // ============================================================================
 impl InitialModel for WindShock {
@@ -40,25 +49,43 @@ impl InitialModel for WindShock {
         Ok(())
     }
 
-    fn primitive_at(&self, coordinate: (f64, f64), _t: f64) -> AnyPrimitive {
+    fn primitive_at(&self, coordinate: (f64, f64), t: f64) -> AnyPrimitive {
         // u: gamma-beta-c
         // v: beta-c
         // rho: comoving rest-mass density
         // Mdot = 4 pi r^2 rho u c
 
-        let r = coordinate.0;
-        let u = if r < self.shock_location {
-            self.wind_gamma_beta
+        if (t >= self.flare_time) & (t < (self.flare_time + 0.1)) {
+            let r = coordinate.0;
+            let u = self.flare_gamma_beta;
+            let rho = self.flare_outflow_rate / (4.0 * PI * r * r * u * LIGHT_SPEED);
+            //let rho = (1.0 - n) / (0.1) * (t - self.flare_time - 0.1) + 1.0;
+            let p = rho * UNIFORM_TEMPERATURE;
+            AnyPrimitive {
+                velocity_r: u,
+                velocity_q: 0.0,
+                mass_density: rho,
+                gas_pressure: p,
+            }
         } else {
-            self.post_shock_gamma_beta
-        };
-        let rho = self.wind_mass_outflow_rate / (4.0 * PI * r * r * u * LIGHT_SPEED);
-
-        AnyPrimitive {
-            velocity_r: u,
-            velocity_q: 0.0,
-            mass_density: rho,
-            gas_pressure: rho * UNIFORM_TEMPERATURE,
+            let r = coordinate.0;
+            let u = if r < self.shock_location {
+                self.wind_gamma_beta
+            } else {
+                self.post_shock_gamma_beta
+            };
+            let rho = self.wind_mass_outflow_rate / (4.0 * PI * r * r * u * LIGHT_SPEED);
+            let p = if r < self.shock_location {
+                self.wind_pressure
+            } else {
+                self.post_shock_pressure
+            };
+            AnyPrimitive {
+                velocity_r: u,
+                velocity_q: 0.0,
+                mass_density: rho,
+                gas_pressure: p,
+            }
         }
     }
 
